9ea4be586f8aebead84ad1afdf7cb110825fe16a
Margo Lanier <lanier.margo@gmail.com>
2017-01-24 19:28:38 -0500

    add old chatterbox styles into backbone structure

--- js/views/chat.js
+++ js/views/chat.js
@@ -2,8 +2,8 @@ module.exports = Backbone.View.extend({
 	
 	initialize() {
 		this.model.on('change', this.render, this);
-		this.model.on('add', this.render, this);
-		this.model.on('remove', this.render, this);
+		//this.model.on('add', this.render, this);
+		//this.model.on('remove', this.render, this);
 	},
 	
 	events: {
